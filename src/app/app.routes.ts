1cd49a044c5a03ddb0a23384cdd3b3075546b71a
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-10-24 18:26:44 +0200

    refactor: update module paths on config files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
   {
     path: 'app',
-    loadChildren: () => import('./features/tasks').then((c) => c.TASKS_ROUTES),
+    loadChildren: () => import('@feature/tasks').then((c) => c.TASKS_ROUTES),
   },
   {
     path: '',
